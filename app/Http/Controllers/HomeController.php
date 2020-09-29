27e5d2d7fef199a24748ed573b4d337d174a7ccb
fahrulsanjaya <fahrursanjaya@gmail.com>
2020-09-29 20:57:25 +0700

    add rating maps finish

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Rating;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,10 +16,22 @@ class HomeController extends Controller
     public function maps()
     {
         $maps=DB::table('sellers')
-                ->select('sellers.nama_seller as name','sellers.latitude as lat','sellers.longitude as lng','sellers.deskripsi','users.foto as image')
+                ->select('sellers.id','sellers.nama_seller as name','sellers.latitude as lat','sellers.longitude as lng','sellers.deskripsi','users.foto as image')
                 ->leftJoin('users','users.id','sellers.id_user')->get();
+        $datamap=array();
+        foreach($maps as $map){
+            $rating=Rating::where('id_seller',$map->id)->avg('rating');
+            $datamap[]=[
+                'name'=>$map->name,
+                'lat'=>$map->lat,
+                'lng'=>$map->lng,
+                'deskripsi'=>$map->deskripsi,
+                'image'=>$map->image,
+                'rating'=>$rating,
+            ];
+        }
         return response([
-            'offices'=>$maps
+            'offices'=>$datamap
         ]);
     }
 }
---
+++ database/migrations/2020_09_29_133455_create_table_ratings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableRatings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_seller');
+            $table->foreignId('id_user');
+            $table->double('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
