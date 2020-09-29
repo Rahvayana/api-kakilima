6cfd64c35c1a1dcc8b2a0a11353716341c6a40d6
fahrulsanjaya <fahrursanjaya@gmail.com>
2020-09-29 20:17:24 +0700

    rev 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     public function maps()
     {
         $maps=DB::table('sellers')
-                ->select('sellers.nama_seller as name','sellers.latitude as lat','sellers.longitude as lng','sellers.deskripsi','users.foto')
+                ->select('sellers.nama_seller as name','sellers.latitude as lat','sellers.longitude as lng','sellers.deskripsi','users.foto as image')
                 ->leftJoin('users','users.id','sellers.id_user')->get();
         return response([
             'offices'=>$maps
